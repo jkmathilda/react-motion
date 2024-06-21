c401ac2e4fc9e55c2b0188fe6610e77bcd9996ae
jkmathilda <jkmathilda627@gmail.com>
2024-06-21 23:40:46 +0900

    Add App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import "./App.css";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,21 +12,63 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
+  width: 300px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255, 245, 245, 0.6);
+  border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  margin: 10px;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <div className="all_boxes">
+        <div className="row">
+          <Box
+            transition={{ type: "spring", delay: 0.5 }}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, rotateZ: 360 }}
+            whileHover={{
+              scale: 1.1,
+              x: "-10%",
+              y: "-10%",
+            }}
+          />{" "}
+          <Box
+            transition={{ type: "spring", delay: 0.5 }}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, rotateZ: 360 }}
+            whileHover={{
+              scale: 1.1,
+              x: "10%",
+              y: "-10%",
+            }}
+          />
+        </div>
+        <div className="row">
+          <Box
+            transition={{ type: "spring", delay: 0.5 }}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, rotateZ: 360 }}
+            whileHover={{
+              scale: 1.1,
+              x: "-10%",
+              y: "10%",
+            }}
+          />
+          <Box
+            transition={{ type: "spring", delay: 0.5 }}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, rotateZ: 360 }}
+            whileHover={{
+              scale: 1.1,
+              x: "10%",
+              y: "10%",
+            }}
+          />
+        </div>
+      </div>
     </Wrapper>
   );
 }
